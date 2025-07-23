396080a4f3964b65d85bd622b4e7fa8b73f5cfba
Harry <hchahuayo.241ds10@istta.edu.pe>
2025-07-23 09:04:22 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,14 +13,14 @@ if (!isset($_SESSION['username'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Control de Gastos</title>
+    <title>FinanzasPro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../dashboard.php">Control de Gastos</a>
+    <a class="navbar-brand" href="../dashboard.php">FinanzasPro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
