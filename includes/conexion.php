da47c6ea0f616d474b36d0bfd97c020ac229aaee
Harry <hchahuayo.241ds10@istta.edu.pe>
2025-07-22 19:11:22 -0500

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,16 +1,13 @@
 <?php
-// Datos de conexión a PostgreSQL
-$host = "localhost";
-$port = "5432";
-$dbname = "control_gastos";
-$user = "postgres";
-$password = "123456"; // CAMBIA esto por tu contraseña real
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT") ?: 5432;
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASS");
 
 try {
     $conexion = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    // Establecer el modo de error de PDO a excepción
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conexión exitosa";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
     exit();
